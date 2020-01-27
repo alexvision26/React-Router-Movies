7dd4929fd83b1fad7c031cc5ee0b9c97e15b0934
Alex Miller <alexmillerprojects@gmail.com>
2020-01-27 15:00:03 -0800

    set up imports and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,7 +14,10 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route exact path='/'/>
+      <Route path ='/movies'/>
     </div>
+    
   );
 };
 
